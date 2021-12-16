a6be302a94653d8bf39af99d7ab44ff23a6a6699
manuelsaileramena <59960861+manuelsaileramena@users.noreply.github.com>
2021-12-16 15:07:47 +0100

    Delete Gruppenleiter.py

--- Firma Github/Gruppenleiter.py
+++
@@ -1,7 +0,0 @@
-from Person import Person
-
-class Gruppenleiter(Person):
-    def __init__(self, firma, abteilung, vorname, nachname, geschlecht, gruppe, gruppenleiterId):
-        super().__init__(firma, abteilung, vorname, nachname, geschlecht, gruppe)
-        self.gruppenleiterId = gruppenleiterId
-
